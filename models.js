f8a1af02db820c8f8f75a11371fd5f3fe5ef9dc6
markallenramirez <markallenramirez@gmail.com>
2020-03-11 22:43:51 +0800

    Roll back

--- models.js
+++ models.js
@@ -4,8 +4,7 @@ let e = {};
 e.database = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
 	dialect: 'sqlite',
 	logging: false,
-	storage: 'database.sqlite',
-	host: 'cloudinary://866841438624761:XSEoRmb5iAwELuAdGIh4zf3Evis@hq0ppy0db'
+	storage: 'data/database.sqlite',
 });
 // # Database Models
 e.Relic = e.database.import('data/models/relic');
